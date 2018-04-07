0bc995e0075e69ff49fcdd1fb81afe439bfd5230
kaihongChan <291560152@qq.com>
2018-04-07 16:43:00 +0800

    引入adminlte

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -10,8 +10,8 @@
             <small>Optional description</small>
         </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Here</li>
+            <li><a href="#"><i class="fa fa-home"></i> home</a></li>
+            <li class="active">index</li>
         </ol>
     </section>
 

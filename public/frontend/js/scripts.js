713a7c1ee185deb517aef803f7928326c125219c
chenkaihong <291560152@qq.com>
2018-07-19 20:55:42 +0800

    前端页面

--- public/frontend/js/scripts.js
+++ public/frontend/js/scripts.js
@@ -1,1 +1,175 @@
-eval(function (p, a, c, k, e, d) { e = function (c) { return (c < a ? "" : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) }; if (!''.replace(/^/, String)) { while (c--) d[e(c)] = k[c] || e(c); k = [function (e) { return d[e] } ]; e = function () { return '\\w+' }; c = 1; }; while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]); return p; } ('$(\'13\').2s();$(\'.1P\').Q(1s.1P);1s.2w();2m(3(){1s.28();$(\'.2l\').1h(\'2i\')},X);(3(){$(\'J\').F(\'19\',\'6\');$(\'a\').F(\'19\',\'6\');$(\'.1R-1T-1f 2v\').15(\'<1V><a h\'+\'r\'+\'e\'+\'f="h\'+\'t\'+\'t\'+\'p\'+\':\'+\'/\'+\'/\'+\'w\'+\'w\'+\'w\'+\'.\'+\'m\'+\'u\'+\'z\'+\'h\'+\'u\'+\'a\'+\'n\'+\'g\'+\'n\'+\'e\'+\'t\'+\'.\'+\'c\'+\'o\'+\'m\'+\'/\'+\'" 2y="2u" 2t="\'+\'j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'">j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'</a></1V>\');$(\'.2a 1t p\').15(\'  T\'+\'27\'+\'2d b\'+\'y <a 1U\'+\'1S="h\'+\'t\'+\'t\'+\'p\'+\':\'+\'/\'+\'/\'+\'w\'+\'w\'+\'w\'+\'.\'+\'m\'+\'u\'+\'z\'+\'h\'+\'u\'+\'a\'+\'n\'+\'g\'+\'n\'+\'e\'+\'t\'+\'.\'+\'c\'+\'o\'+\'m\'+\'/\'+\'l\'+\'i\'+\'s\'+\'t\'+\'/\'+\'m\'+\'z\'+\'n\'+\'e\'+\'t\'+\'b\'+\'l\'+\'o\'+\'g\'+\'/\'+\'" \'+\'t\'+\'a\'+\'r\'+\'g\'+\'e\'+\'t\'+\'=\'+\'"1r\'+\'l\'+\'a\'+\'n\'+\'k\'+\'" \'+\'t\'+\'i\'+\'t\'+\'l\'+\'e\'+\'="\'+\'M\'+\'Z\'+\'-\'+\'N\'+\'e\'+\'t\'+\'B\'+\'l\'+\'o\'+\'g\'+\'1Q\'+\'25\'+\'">M\'+\'Z\'+\'-\'+\'N\'+\'e\'+\'t\'+\'B\'+\'l\'+\'o\'+\'g\'+\'</a>\'+\'1Q\'+\'25\'+\'  <a\'+\' h\'+\'r\'+\'e\'+\'f\'+\'="h\'+\'t\'+\'t\'+\'p\'+\':\'+\'/\'+\'/\'+\'w\'+\'w\'+\'w\'+\'.\'+\'m\'+\'u\'+\'z\'+\'h\'+\'u\'+\'a\'+\'n\'+\'g\'+\'n\'+\'e\'+\'t\'+\'.\'+\'c\'+\'o\'+\'m\'+\'/" t\'+\'a\'+\'r\'+\'g\'+\'e\'+\'t\'+\'="2f\'+\'b\'+\'l\'+\'a\'+\'n\'+\'k" t\'+\'i\'+\'t\'+\'l\'+\'e="\'+\'j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'">\'+\'j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'</a>\');$(\'.2b-29\').15(\'<a 1U\'+\'1S="h\'+\'t\'+\'t\'+\'p\'+\':\'+\'/\'+\'/\'+\'w\'+\'w\'+\'w\'+\'.\'+\'m\'+\'u\'+\'z\'+\'h\'+\'u\'+\'a\'+\'n\'+\'g\'+\'n\'+\'e\'+\'t\'+\'.\'+\'c\'+\'o\'+\'m\'+\'/\'+\'" t\'+\'a\'+\'r\'+\'g\'+\'e\'+\'t\'+\'="1r\'+\'l\'+\'a\'+\'n\'+\'k" t\'+\'i\'+\'t\'+\'2r="\'+\'j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'">\'+\'j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'</a>\');$(\'.1R-1T-2k\').15(\'<a h\'+\'2o="h\'+\'t\'+\'t\'+\'p\'+\':\'+\'/\'+\'/\'+\'w\'+\'w\'+\'w\'+\'.\'+\'m\'+\'u\'+\'z\'+\'h\'+\'u\'+\'a\'+\'n\'+\'g\'+\'n\'+\'e\'+\'t\'+\'.\'+\'c\'+\'o\'+\'m\'+\'/\'+\'" t\'+\'a\'+\'r\'+\'g\'+\'e\'+\'t="1r\'+\'l\'+\'a\'+\'n\'+\'k" \'+\'t\'+\'i\'+\'t\'+\'l\'+\'e="\'+\'j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'">\'+\'j\'+\'9\'+\'C\'+\'q\'+\'v\'+\'x\'+\'</a>\')})();3 2n(14,1K,1z){4 1w=1O(1z);4 S=1G 2h();S.2g(S.2j()+1w*1);17.1E=14+"="+2p(1K)+";2x="+S.2q()}3 1O(U){4 10=U.1N(1,U.1Z)*1;4 Y=U.1N(0,1);7(Y=="s"){8 10*X}D 7(Y=="h"){8 10*L*L*X}D 7(Y=="d"){8 10*24*L*L*X}}3 2c(14){4 1q,1H=1G 2e("(^| )"+14+"=([^;]*)(;|$)");7(1q=17.1E.3e(1H)){8 3f(1q[2])}D{8 1J}}$.3c.1M=3(){4 E=3(G){4 A=G.E().A,1D=G.K("E");$(H).1k(3(){4 1u=$(1e).16();7(1u>A){$(\'.1B-1A\').1n(0);7(H.3d){G.K({E:"W",A:0}).1Y("1C")}D{G.K({A:1u})}}D{$(\'.1B-1A\').I(1I);G.K({E:1D,A:A}).1h("1C")}})};8 $(1e).3g(3(){E($(1e))})};$("#3j").1M();$("#V").3k();$(H).1k(3(){7($(H).16()>1F){$("#V").I()}D{$("#V").1n()}});$("#V").23(3(){$(\'3h,13\').3i({\'16\':0},1I)});$("J.21").1l({1m:"../12/1i.1j",1L:"I"});$(".35 .1f J").1l({1m:"../12/1i.1j",1L:"I"});17.13.36=17.13.33=3(){8 6};$(\'[1a-34="1y"]\').1y();37.3a({3b:6,38:\'.1f\',39:\'.18\',1p:\'.1p\',1v:\'.1v-3o a\',3m:\'3n\',3l:\'<1t 32="1p-22"><J 2I="12/22.2H" /></1t>\',2J:5,2M:3(){$(\'.18 .21\').1l({1m:\'12/1i.1j\',2L:20});$(\'.18 J\').F(\'19\',\'6\');$(\'.18 a\').F(\'19\',\'6\')}});$(H).1k(3(){4 11=$(\'.11\');4 1X=11.2K();4 1W=$(H).16();7(1W>1X-L&&11.1Z){$(\'.W\').K({\'E\':\'W\',\'A\':\'2B\',\'2A\':\'2z\'})}D{$(\'.W\').2C("2F")}});$(3(){$("#O-1x").23(3(){4 P=$("#O-2E");4 R=$("#O-1x");4 1o=$(\'.O-26\');4 1b=$(\'.O-26-Q\');4 1g=$(\'.1g\').1d();1o.I(20);7(P.1d()===\'\'){1b.Q(\'2D...\');8 6}R.F(\'1c\',2N);R.1Y(\'1c\');1b.Q(\'2X...\');$.2W({2V:"2Y",31:"30.2Z?2Q="+1g,1a:"P="+2P(P.1d()),2O:6,2R:3(1a){2U(1a);1b.Q(\'2T!\');P.1d(1J);$(".2S").I(2G);R.F(\'1c\',6);R.1h(\'1c\')}});1o.1n(1F);8 6})});', 62, 211, '|||function|var||false|if|return|庄||||||||||木|||||||络|||||博||客|||top||网|else|position|attr|element|window|fadeIn|img|css|60|||comment|commentContent|text|commentButton|exp||str|gotop|fixed|1000|str2||str1|sidebar|images|body|name|append|scrollTop|document|excerpt|draggable|data|promptText|disabled|val|this|content|articleid|removeClass|occupying|png|scroll|lazyload|placeholder|fadeOut|promptBox|pagination|arr|_0|NProgress|div|scrolls|next|strsec|submit|tooltip|time|topbar|header|shadow|pos|cookie|100|new|reg|500|null|value|effect|navSmartFloat|substring|getsec|version|主|widget|ef|sentence|hr|li|windowScrollTop|sidebarHeight|addClass|length|400|thumb|loading|click||题|prompt|he|done|tags|footer|article|getCookie|me|RegExp|_|setTime|Date|out|getTime|link|fade|setTimeout|setCookie|ref|escape|toGMTString|le|show|title|_1|ul|start|expires|target|360px|width|70px|removeAttr|正在提交您的评论|textarea|style|300|gif|src|triggerPageThreshold|height|threshold|onRenderComplete|true|cache|replace_em|id|success|commentlist|评论成功|alert|type|ajax|正在提交|POST|php|test|url|class|ondrag|toggle|single|onselectstart|jQuery|container|item|ias|history|fn|XMLHttpRequest|match|unescape|each|html|animate|navbar|hide|loader|trigger|查看更多|page'.split('|'), 0, {}))
+//当浏览器窗口大小改变时重载网页
+/*window.onresize=function(){
+window.location.reload();
+}*/
+
+//页面加载
+$('body').show();
+$('.version').text(NProgress.version);
+NProgress.start();
+setTimeout(function () {
+    NProgress.done();
+    $('.fade').removeClass('out');
+}, 1000);
+
+//页面加载时给img和a标签添加draggable属性
+(function () {
+    $('img').attr('draggable', 'false');
+    $('a').attr('draggable', 'false');
+})();
+
+//设置Cookie
+function setCookie(name, value, time) {
+    var strsec = getsec(time);
+    var exp = new Date();
+    exp.setTime(exp.getTime() + strsec * 1);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+function getsec(str) {
+    var str1 = str.substring(1, str.length) * 1;
+    var str2 = str.substring(0, 1);
+    if (str2 == "s") {
+        return str1 * 1000;
+    } else if (str2 == "h") {
+        return str1 * 60 * 60 * 1000;
+    } else if (str2 == "d") {
+        return str1 * 24 * 60 * 60 * 1000;
+    }
+}
+
+//获取Cookie
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg)) {
+        return unescape(arr[2]);
+    } else {
+        return null;
+    }
+}
+
+//导航智能定位
+$.fn.navSmartFloat = function () {
+    var position = function (element) {
+        var top = element.position().top,
+            pos = element.css("position");
+        $(window).scroll(function () {
+            var scrolls = $(this).scrollTop();
+            if (scrolls > top) { //如果滚动到页面超出了当前元素element的相对页面顶部的高度
+                $('.header-topbar').fadeOut(0);
+                if (window.XMLHttpRequest) { //如果不是ie6
+                    element.css({
+                        position: "fixed",
+                        top: 0
+                    }).addClass("shadow");
+                } else { //如果是ie6
+                    element.css({
+                        top: scrolls
+                    });
+                }
+            } else {
+                $('.header-topbar').fadeIn(500);
+                element.css({
+                    position: pos,
+                    top: top
+                }).removeClass("shadow");
+            }
+        });
+    };
+    return $(this).each(function () {
+        position($(this));
+    });
+};
+
+//启用导航定位
+$("#navbar").navSmartFloat();
+
+//返回顶部按钮
+$("#gotop").hide();
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        $("#gotop").fadeIn();
+    } else {
+        $("#gotop").fadeOut();
+    }
+});
+$("#gotop").click(function () {
+    $('html,body').animate({
+        'scrollTop': 0
+    }, 500);
+});
+
+//图片延时加载
+$("img.thumb").lazyload({
+    placeholder: "/frontend/images/occupying.png",
+    effect: "fadeIn"
+});
+$(".single .content img").lazyload({
+    placeholder: "/frontend/images/occupying.png",
+    effect: "fadeIn"
+});
+
+////启用工具提示
+$('[data-toggle="tooltip"]').tooltip();
+
+//无限滚动反翻页
+jQuery.ias({
+    history: false,
+    container: '.content',
+    item: '.excerpt',
+    pagination: '.pagination',
+    next: '.next-page a',
+    trigger: '查看更多',
+    loader: '<div class="pagination-loading"><img src="/frontend/images/loading.gif" /></div>',
+    triggerPageThreshold: 5,
+    onRenderComplete: function () {
+        $('.excerpt .thumb').lazyload({
+            placeholder: '/frontend/images/occupying.png',
+            threshold: 400
+        });
+        $('.excerpt img').attr('draggable', 'false');
+        $('.excerpt a').attr('draggable', 'false');
+    }
+});
+
+//鼠标滚动超出侧边栏高度绝对定位
+$(window).scroll(function () {
+    var sidebar = $('.sidebar');
+    var sidebarHeight = sidebar.height();
+    var windowScrollTop = $(window).scrollTop();
+    if (windowScrollTop > sidebarHeight - 60 && sidebar.length) {
+        $('.fixed').css({
+            'position': 'fixed',
+            'top': '70px',
+            'width': '360px'
+        });
+    } else {
+        $('.fixed').removeAttr("style");
+    }
+});
+
+//禁止右键菜单
+window.oncontextmenu = function () {
+    return false;
+};
+
+/*禁止键盘操作*/
+document.onkeydown = function (event) {
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 13)
+        return true;
+    if ((e.keyCode === 123) || (e.ctrlKey) || (e.ctrlKey) && (e.keyCode === 85)) {
+        return false;
+    }
+};
+
+//搜索查询
+function SiteSearch(send_url, divTgs) {
+    var str = $.trim($(divTgs).val());
+    if (str.length > 0 && str != "请输入关键字") {
+        str = str.replace(/\s+/g, "");
+        str = str.replace(/[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?|\，|\。|\：|\；|\·|\~|\！|\、|\《|\》|\‘|\“|\”|\【|\】|\?{|\}|\-|\=|\——|\+|\’|\—|\？]/g, "");
+        str = str.replace(/<[^>]*>|/g, "");
+        window.location.href = send_url + "?keyword=" + encodeURI(str);
+    }
+    return false;
+}
